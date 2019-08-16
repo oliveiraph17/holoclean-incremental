9436c3372f21de247b19f8da8b124cdf5551fb6f
PH <oliveiraph17@gmail.com>
2019-08-16 00:16:04 -0400

    Adjusts script file.

--- examples/start_example.sh
+++ examples/start_example.sh
@@ -2,20 +2,20 @@
 source ../set_env.sh
 
 script="baseline_2.py"
-python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../Experiments/hospital_2_batches/baseline_2.txt
+python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../experiments/hospital_2_batches/baseline_2.txt
 
 script="proposal_3.py"
-python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../Experiments/hospital_2_batches/proposal_3.txt
+python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../experiments/hospital_2_batches/proposal_3.txt
 
 script="proposal_4.py"
-python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../Experiments/hospital_2_batches/proposal_4.txt
+python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../experiments/hospital_2_batches/proposal_4.txt
 
 script="proposal_5.py"
-python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../Experiments/hospital_2_batches/proposal_5.txt
+python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../experiments/hospital_2_batches/proposal_5.txt
 
 script="proposal_6.py"
-python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../Experiments/hospital_2_batches/proposal_6.txt
+python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../experiments/hospital_2_batches/proposal_6.txt
 
 script="proposal_7.py"
-python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../Experiments/hospital_2_batches/proposal_7.txt
+python $script 2>&1 >/dev/null | grep 'EXPERIMENT' >> ../experiments/hospital_2_batches/proposal_7.txt
 
