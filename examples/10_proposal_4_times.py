efa764de3be7b788b9354516827512dd53707bb8
PH <oliveiraph17@gmail.com>
2019-08-17 23:50:58 -0400

    Adjusts script files.

--- examples/10_proposal_4_times.py
+++ examples/10_proposal_4_times.py
@@ -9,29 +9,24 @@ sys.path.append('../')
 dataset_name = 'hospital'
 batches = ['0001-0100', '0101-0200', '0201-0300', '0301-0400', '0401-0500',
            '0501-0600', '0601-0700', '0701-0800', '0801-0900', '0901-1000']
-batch_stopping_training = 7
-skip_training = False
 
-number_of_iterations = 1
+number_of_iterations = 10
 
-log_repairing_quality = True
-log_execution_times = False
+log_repairing_quality = False
+log_execution_times = True
 log_fpath = ''
 
 if log_repairing_quality:
-    log_fpath += '/home/ph/Git/HoloClean/experiments/hospital_10_batches/repairing_quality/proposal_5.csv'
+    log_fpath += '/home/ph/Git/HoloClean/experiments/hospital_10_batches/repairing_quality/proposal_4.csv'
 
 if log_execution_times:
-    log_fpath += '/home/ph/Git/HoloClean/experiments/hospital_10_batches/execution_times/proposal_5.csv'
+    log_fpath += '/home/ph/Git/HoloClean/experiments/hospital_10_batches/execution_times/proposal_4.csv'
 
 for current_iteration in range(number_of_iterations):
     current_batch_number = 0
 
     # We may run out of memory if HoloClean is not reinstantiated at each loading step.
     for current_batch in batches:
-        if current_batch_number == batch_stopping_training:
-            skip_training = True
-
         # Sets up a HoloClean session.
         hc = holoclean.HoloClean(
             db_name='holo',
@@ -60,7 +55,7 @@ for current_iteration in range(number_of_iterations):
             default_entropy=False,
             repair_previous_errors=True,
             recompute_from_scratch=False,
-            skip_training=skip_training,
+            skip_training=False,
             ignore_previous_training_cells=False,
             save_load_checkpoint=True
         ).session
