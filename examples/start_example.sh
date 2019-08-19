16e78dc5dfd4574d88c0fc9ddcca1f4bb901a850
PH <oliveiraph17@gmail.com>
2019-08-19 09:26:02 -0400

    Includes all results from the hospital dataset.

--- examples/start_example.sh
+++ examples/start_example.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 source ../set_env.sh
 
-script="10_baseline_2.py"
+script="10_proposal_6_times.py"
 python $script 2>/dev/null
 
-script="10_proposal_3.py"
+script="10_proposal_7_times.py"
 python $script 2>/dev/null
 
-script="10_proposal_4.py"
-python $script 2>/dev/null
-
-script="10_proposal_5.py"
-python $script 2>/dev/null
-
-script="10_proposal_6.py"
-python $script 2>/dev/null
-
-script="10_proposal_7.py"
-python $script 2>/dev/null
