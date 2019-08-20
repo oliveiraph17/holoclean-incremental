ac9fef2082f73ab3d8a5c0f8db16effd1941d698
PH <oliveiraph17@gmail.com>
2019-08-19 21:30:47 -0400

    Updates script file to run only the Proposal 5 for computing execution times.

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,41 +5,47 @@ source ../set_env.sh
 # Repairing quality
 ################################################################################
 
-dir_path="adult_1/one_shot"
-
-script_file="baseline_1.py"
-script_path="$dir_path/$script_file"
-python ${script_path} 2>/dev/null
-
-dir_path="adult_1/4_batches"
-
-script_file="baseline_2.py"
-script_path="$dir_path/$script_file"
-python ${script_path} 2>/dev/null
-
-script_file="proposal_3.py"
-script_path="$dir_path/$script_file"
-python ${script_path} 2>/dev/null
-
-script_file="proposal_4.py"
-script_path="$dir_path/$script_file"
-python ${script_path} 2>/dev/null
-
-script_file="proposal_5.py"
-script_path="$dir_path/$script_file"
-python ${script_path} 2>/dev/null
-
-script_file="proposal_6.py"
-script_path="$dir_path/$script_file"
-python ${script_path} 2>/dev/null
-
-script_file="proposal_7.py"
-script_path="$dir_path/$script_file"
-python ${script_path} 2>/dev/null
+#dir_path="adult_1/one_shot"
+#
+#script_file="baseline_1.py"
+#script_path="$dir_path/$script_file"
+#python ${script_path} 2>/dev/null
+#
+#dir_path="adult_1/4_batches"
+#
+#script_file="baseline_2.py"
+#script_path="$dir_path/$script_file"
+#python ${script_path} 2>/dev/null
+#
+#script_file="proposal_3.py"
+#script_path="$dir_path/$script_file"
+#python ${script_path} 2>/dev/null
+#
+#script_file="proposal_4.py"
+#script_path="$dir_path/$script_file"
+#python ${script_path} 2>/dev/null
+#
+#script_file="proposal_5.py"
+#script_path="$dir_path/$script_file"
+#python ${script_path} 2>/dev/null
+#
+#script_file="proposal_6.py"
+#script_path="$dir_path/$script_file"
+#python ${script_path} 2>/dev/null
+#
+#script_file="proposal_7.py"
+#script_path="$dir_path/$script_file"
+#python ${script_path} 2>/dev/null
 
 ################################################################################
 # Execution times
 ################################################################################
+
+dir_path="hospital/10_batches"
+
+script_file="proposal_5_times.py"
+script_path="$dir_path/$script_file"
+python ${script_path} 2>/dev/null
 
 #dir_path="adult_1/one_shot"
 #
